8616fc99175aafc443f8ecf848603be1168527ef
mishaberrien <markishab@gmail.com>
2019-08-20 21:01:51 -0400

    updated requirements with pandas to see if it would change anything

--- src/setup.py
+++ src/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='src',
       version='0.0.1',
       description='test',
-      author='Misha',
+      author='Misha Berrien',
       author_email='misha.berrien@gmail.com',
       packages=['my_third_package'])
